c3026d2d560a3f8bf5d0e8e0c5f3a4d81d4944ff
kenechukwu <casmirvictor293@gmail.com>
2022-12-09 12:10:13 +0100

    initialized reo

---
+++ app.js
@@ -0,0 +1,34 @@
+var myName = "kingsey";
+ let myName2 = "Etoka";     //anything that can change in time are var and let
+const middleName = "Ebube"; //while const as constant is never changing
+console.log(myName)
+myName = "Henry";
+
+
+
+
+
+// string is anything that is wrapped in double or single quote
+//""
+// number no number is written inside a double quote or single quote write it as it is as far as its a number
+let age = 40;
+// boolean data type  is written an either true or false it has to be one out of two possible state true or false
+let isEmployed = "false";
+console.log(isEmployed)
+// null 
+//age = null;
+// undefined is when nothing is dded just the let and there is no information 
+//let bag; 
+// array usually we start counting from 0 anything less thn one is undefined
+                 // index0    index1   idex1 
+let teamLeads = ["Elon Musk","pinky","kene",]
+console.log(teamLeads[0]);
+// object mirror off real life entities like a blue print of a real life intity
+let humanFeature = {
+    noOfHead:2 ,
+    noOfArms:7,
+    noOfLegs:24
+}
+console.log(humanFeature.noOfArms)
+
+console.log(i am a man)
---
+++ learn.js
@@ -0,0 +1,26 @@
+var myName = "victor";
+console.log(myName)
+//NEVER FORGET TO USE THE CAMEL CASING
+
+myName = "kj";
+console.log(myName)
+
+let ourName = "freecodecamp";
+console.log(ourName)
+ourName = "kilo";
+console.log(ourName)
+const pi=3.14;
+/* JAVASCRIPT can also run mathematical operations using these various symbols*/
+let add = 10 + 10; //to add numbers in js you use the normall  "+" sign
+console.log(add)
+let minus = 40 - 33; //same goes for minus
+console.log(minus)
+let multiply = 100*25; //then to multiply we use asterics or
+console.log(multiply)
+let divide = 100/10;
+console.log(divide) 
+// incrementing a number by adding 1 to the name of the variable and minusing by doing the exact same thing
+var myVar = 9;
+myVar = myVar +1;
+console.log(myVar)
+//you can also multiply decimals using the same method as plus and minus
